02833822144ea5629f157d787756af7480d7cf43
lconstjs <lconstante@seveti.com>
2019-04-14 00:24:35 -0700

    added new carousel

---
+++ src/app/views/carousel-details/carousel-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-details',
+  templateUrl: './carousel-details.component.html',
+  styleUrls: ['./carousel-details.component.scss']
+})
+export class CarouselDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
